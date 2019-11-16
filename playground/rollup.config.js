2dc5045c66d78d864aaddc72d6a74b622e9f1345
Vladimir Yangurskiy <vladyang@amazon.com>
2019-11-15 23:56:12 -0800

    adding rollup files

---
+++ playground/rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import rollup_start_dev from './rollup_start_dev';
+import typescript from 'rollup-plugin-typescript';
+
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: 'src/main.ts',
+    output: {
+        sourcemap: true,
+        format: 'iife',
+        name: 'app',
+        file: 'public/bundle.js'
+    },
+    plugins: [
+	typescript(),
+
+        // If you have external dependencies installed from
+        // npm, you'll most likely need these plugins. In
+        // some cases you'll need additional configuration —
+        // consult the documentation for details:
+        // https://github.com/rollup/rollup-plugin-commonjs
+        resolve({
+            browser: true,
+            dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+        }),
+        commonjs(),
+
+        // In dev mode, call `npm run start:dev` once
+        // the bundle has been generated
+        !production && rollup_start_dev,
+
+        // Watch the `public` directory and refresh the
+        // browser on changes when not in production
+        !production && livereload('public'),
+
+        // If we're building for production (npm run build
+        // instead of npm run dev), minify
+        production && terser()
+    ],
+    watch: {
+        clearScreen: false
+    }
+};

1f1a278d69fae234ccebfd71af7b244489cb3981
TTRider <software@ttrider.com>
2020-03-11 18:27:28 -0700

    fixing tests

--- tests/template-handler/test-end2end.ts
+++ tests/template-handler/test-end2end.ts
@@ -2,12 +2,14 @@ import path from "path";
 import { ComponentFile } from "../../src/component-file";
 import processComponentFile from "../../src/defaultHandlers/file-handler";
 import processTemplate from "../../src/defaultHandlers/template-handler";
+import { formatComponentFileObject } from "../utils";
 
 
 describe("msx-end2end", () => {
     test("t00", async () => {
         const { componentFile } = await setup("t00");
-        //expect(componentFile).toMatchSnapshot();
+        const cf = formatComponentFileObject(componentFile);
+        expect(cf).toMatchSnapshot();
     });
 
 });
@@ -31,8 +33,11 @@ async function setup(fileName: string, id: string = "t01") {
 
     const component = componentFile.components[id];
 
+    const formatted = formatComponentFileObject(componentFile);
+
     return {
         componentFile,
-        component
+        component,
+        formatted
     }
 }
